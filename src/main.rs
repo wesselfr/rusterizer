98964fd1a7de359e9d71f330954e3e01d23ee0b0
Wesselfr <wessel.frijters@gmail.com>
2022-02-03 23:23:10 +0100

    Added plotline

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 extern crate minifb;
 
-use glam::{Mat2, Vec2, Vec3};
+use glam::{Vec2};
 use minifb::{Key, Window, WindowOptions};
 
 const WIDTH: usize = 640;
 const HEIGHT: usize = 640;
+const DEBUG_COLOR: Color = Color {
+    a: 255,
+    r: 255,
+    g: 0,
+    b: 255,
+};
 
 pub fn edge_function_cw(v0: Vec2, v1: Vec2, p: Vec2) -> f32 {
     (p.x - v0.x) * (v1.y - v0.y) - (p.y - v0.y) * (v1.x - v0.x)
 }
 
 pub fn index_to_coords(p: usize, height: usize) -> (usize, usize) {
-    (p % height, p / height)
+    (p / height, p % height)
 }
 
 pub fn to_argb8(a: u8, r: u8, g: u8, b: u8) -> u32 {
@@ -22,25 +28,89 @@ pub fn to_argb8(a: u8, r: u8, g: u8, b: u8) -> u32 {
     argb
 }
 
+pub struct Color {
+    pub a: u8,
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+impl Color {
+    pub fn to_argb8(&self) -> u32 {
+        let mut argb: u32 = self.a as u32;
+        argb = (argb << 8) + self.r as u32;
+        argb = (argb << 8) + self.g as u32;
+        argb = (argb << 8) + self.b as u32;
+        argb
+    }
+}
+
+fn plotline_low(v0: Vec2, v1: Vec2, color: Color, buff: &mut Vec<u32>) {
+    let dx = v1.x - v0.x;
+    let mut dy = v1.y - v0.y;
+    let mut yi = 1.0;
+    if dy < 0.0 {
+        yi = -1.0;
+        dy = -dy;
+    }
+    let mut d = (2.0 * dy) - dx;
+    let mut y = v0.y;
+
+    for x in v0.x as usize..v1.x as usize {
+        buff[x * WIDTH + y as usize] = color.to_argb8();
+        if d >= 0.0 {
+            y += yi;
+            d += 2.0 * (dy - dx);
+        } else {
+            d += 2.0 * dy
+        }
+    }
+}
+
+fn plotline_high(v0: Vec2, v1: Vec2, color: Color, buff: &mut Vec<u32>) {
+    let mut dx = v1.x - v0.x;
+    let dy = v1.y - v0.y;
+    let mut xi = 1.0;
+    if dx < 0.0 {
+        xi = -1.0;
+        dx = -dx;
+    }
+    let mut d = (2.0 * dx) - dy;
+    let mut x = v0.x;
+
+    for y in v0.y as usize..v1.y as usize {
+        buff[x as usize * WIDTH + y] = color.to_argb8();
+        if d >= 0.0 {
+            x += xi;
+            d += 2.0 * (dx - dy);
+        } else {
+            d += 2.0 * dx;
+        }
+    }
+}
+
+fn plotline(v0: Vec2, v1: Vec2, color: Color, buff: &mut Vec<u32>) {
+    if (v1.y - v0.y).abs() < (v1.x - v0.x).abs() {
+        if v0.x > v1.x {
+            plotline_low(v1, v0, color, buff);
+        } else {
+            plotline_low(v0, v1, color, buff);
+        }
+    } else {
+        if v0.y > v1.y {
+            plotline_high(v1, v0, color, buff);
+        } else {
+            plotline_high(v0, v1, color, buff);
+        }
+    }
+}
+
 fn main() {
-    let vec1 = Vec2::new(20.0, 30.0);
-    let vec2 = Vec2::new(30.0, 60.0);
-
-    let matrix = Mat2::from_cols(vec1, vec2);
-    let det = matrix.determinant();
-    println!("Determinant: {}", det);
-
     let triangle = [
         Vec2::new(100.0, 100.0),
         Vec2::new(250.0, 400.0),
         Vec2::new(400.0, 100.0),
     ];
-    let edge = (Vec2::new(0.0, 0.0), Vec2::new(WIDTH as f32, HEIGHT as f32));
-
-    println!(
-        "Edge function: {}",
-        edge_function_cw(vec1, vec2, Vec2::new(10.0, 20.0))
-    );
 
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
@@ -55,25 +125,27 @@ fn main() {
     });
 
     // Limit to max ~60 fps update rate
-    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+    window.limit_update_rate(Some(std::time::Duration::from_micros(8300)));
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         for i in 0..buffer.len() {
-            let coords = index_to_coords(i as usize, HEIGHT);
+            let coords = index_to_coords(i as usize, WIDTH);
             let coords = Vec2::new(coords.0 as f32, coords.1 as f32);
             let m0 = edge_function_cw(coords, triangle[1], triangle[2]);
             let m1 = edge_function_cw(coords, triangle[2], triangle[0]);
             let m2 = edge_function_cw(coords, triangle[0], triangle[1]);
 
-            //let side = edge_function_cw(coords, edge.0, edge.1);
-            // if m0 > 0.0 && m1 > 0.0 && m2 > 0.0{
-            //     buffer[i] = to_argb8(255, 255, 0, 0);
-            // }
-            // else{
-            //     buffer[i] = to_argb8(255, 0, 0, 0);
-            // }
+            if m0 > 0.0 && m1 > 0.0 && m2 > 0.0 {
+                buffer[i] = to_argb8(255, 255, 0, 0);
+            } else {
+                buffer[i] = to_argb8(255, 0, 0, 0);
+            }
 
-            buffer[i] = to_argb8(255, m0 as u8, m1 as u8, m2 as u8);
+            //buffer[i] = to_argb8(255, m0 as u8, m1 as u8, m2 as u8);
+
+            plotline(triangle[0], triangle[1], DEBUG_COLOR, &mut buffer);
+            plotline(triangle[1], triangle[2], DEBUG_COLOR, &mut buffer);
+            plotline(triangle[2], triangle[0], DEBUG_COLOR, &mut buffer);
         }
 
         // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
